52a9a93148e7d90a8a632ebcb849734ab8e971b4
piyush <hellopiyush.lab@gmail.com>
2026-02-02 03:52:21 +0000

    made a prototype div, and can add the objects into the array now

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// Arrary to store the objects (Books)
 const myLibrary = [];
 
+// Constructor to make the Book instances
 function Book(title, author, status) {
     if (!new.target) {
         console.log("Book() is a constructor. Use with 'new' keyword.");
@@ -9,19 +11,33 @@ function Book(title, author, status) {
     this.status = status;
 }
 
+// main function to invoke the constructor, and
+// add the objects to the Array
 function addBookToLibrary() {
     // this will need a click event
     // take parameters, create a book then store it in the array
 }
 
+// getting the form element, and its sub elements
 const form = document.getElementById("addBookForm");
 const titleInput = document.getElementById("titleInput");
 const authorInput = document.getElementById("authorInput");
 const statusInput = document.getElementById("statusInput");
 
+// the event listener is on the form itself for submission
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    console.log(titleInput.value);
-    console.log(authorInput.value);
-    console.log(statusInput.value);
+    e.preventDefault(); // avoid reloading the page here
+    // use the user input to make the object:
+    const newBook = new Book(titleInput.value, authorInput.value, statusInput.value); 
+    // push the new object into the myLibrary array
+    myLibrary.push(newBook);
+
+    // just for checking for myself:
+    console.log(myLibrary);
 });
+
+// function to iterate over the library,
+// so that it can be shown in web page
+function myLibraryIterator() {
+
+}
